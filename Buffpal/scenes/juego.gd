2784aca1d0e3f3ddc90c1b2d99d88f0dca9d1106
Gx3J <juanjurado.dev@gmail.com>
2023-12-12 09:54:26 -0300

    Arreglo errores de ultimo push
    Arreglo errores de ultimo push

--- Buffpal/scenes/juego.gd
+++ Buffpal/scenes/juego.gd
@@ -3,6 +3,7 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	Global.scene_changes_count = 0
 	Transicion.hide()
 	Transicion2.hide()
 	Transicion3.hide()
